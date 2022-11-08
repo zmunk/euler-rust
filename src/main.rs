6b01a3ec37af297e69f4e39397349d87fe0d1ae9
zmunk <ibrahim.tigrek@gmail.com>
2022-11-08 22:01:42 +0300

    timing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 mod utils;
 mod problem1;
 use problem1 as p;
+use std::time::Instant;
 
 fn main() {
+    let start = Instant::now();
     let res = p::main();
     println!("{:?}", res);
+    println!("elapsed: {:?}", start.elapsed());
 }
